d92c6e458e428c9b97773820841010f98c4e2590
dharsine <120012920+dharsine8@users.noreply.github.com>
2023-10-11 15:20:54 +0530

    Add files via upload

---
+++ Day 18/feedback_service/src/main/java/com/example/demo/dto/FeedbackResponse.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedbackResponse {
+	private String name;
+	private String email;
+	private String message;
+
+}
